cc92f1b11b5d28c89601ff88d5bfce8bc7b80f56
Meghul Parmar <117036095+Meghul@users.noreply.github.com>
2024-07-15 21:14:16 +0530

    Time: 666 ms (80.13%), Space: 281.5 MB (49.78%) - LeetHub

--- 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.cpp
+++ 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.cpp
@@ -1,51 +1,28 @@
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-
-using namespace std;
-
-// Assuming TreeNode is already defined as:
-// struct TreeNode {
-//     int val;
-//     TreeNode *left;
-//     TreeNode *right;
-//     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-// };
-
 class Solution {
-public:
-    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        unordered_map<int, TreeNode*> nodeMap;
-        unordered_set<int> children;
+ public:
+  TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+    unordered_map<TreeNode*, TreeNode*> childToParent;
+    unordered_map<int, TreeNode*> valToNode;
 
-        for (const auto& description : descriptions) {
-            int parentValue = description[0];
-            int childValue = description[1];
-            bool isLeft = description[2];
-
-            if (nodeMap.count(parentValue) == 0) {
-                nodeMap[parentValue] = new TreeNode(parentValue);
-            }
-
-            if (nodeMap.count(childValue) == 0) {
-                nodeMap[childValue] = new TreeNode(childValue);
-            }
-
-            if (isLeft) {
-                nodeMap[parentValue]->left = nodeMap[childValue];
-            } else {
-                nodeMap[parentValue]->right = nodeMap[childValue];
-            }
-
-            children.insert(childValue);
-        }
-
-        for (const auto& [value, node] : nodeMap) {
-            if (children.count(value) == 0) {
-                return node;
-            }
-        }
-
-        return nullptr;
+    for (const vector<int>& d : descriptions) {
+      const int p = d[0];
+      const int c = d[1];
+      const bool isLeft = d[2];
+      TreeNode* parent =
+          valToNode.count(p) ? valToNode[p] : (valToNode[p] = new TreeNode(p));
+      TreeNode* child =
+          valToNode.count(c) ? valToNode[c] : (valToNode[c] = new TreeNode(c));
+      childToParent[child] = parent;
+      if (isLeft)
+        parent->left = child;
+      else
+        parent->right = child;
     }
+
+    // Pick a random node and traverse upwardly.
+    TreeNode* root = childToParent.begin()->second;
+    while (childToParent.count(root))
+      root = childToParent[root];
+    return root;
+  }
 };
