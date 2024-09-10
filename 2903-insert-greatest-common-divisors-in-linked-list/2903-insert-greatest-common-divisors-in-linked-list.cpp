5647207336b1ee61aa0bfb565b6dca72e7b3da61
Meghul Parmar <117036095+Meghul@users.noreply.github.com>
2024-09-10 10:08:14 +0530

    Time: 40 ms (54.53%), Space: 35.6 MB (49.64%) - LeetHub

--- 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.cpp
+++ 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.cpp
@@ -10,30 +10,28 @@
  */
 class Solution {
 public:
-
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
-       if(head==nullptr||head->next==nullptr){
-        return head;
-       } 
-       ListNode *node1=head;
-       ListNode *node2=head->next;
-       
-      while(node2!=nullptr){
-        int gcdval=calculategcd(node1->val,node2->val);
-        ListNode *gcdnode=new ListNode(gcdval);
-        node1->next=gcdnode;
-        gcdnode->next=node2;
+        if(head==nullptr||head->next==nullptr) return head;
+        ListNode *node1=head;
+        ListNode *node2=head->next;
 
-        node1=node2;
-        node2=node2->next;
-      }
-      return head;
+        while(node2!=nullptr){
+            int gcdval=calculategcd(node1->val,node2->val);
+            ListNode *gcdnode= new ListNode(gcdval);
+
+            node1->next=gcdnode;
+            gcdnode->next=node2;
+
+            node1=node2;
+            node2=node2->next;
+        }
+        return head;
     }
     private:
-int calculategcd(int a,int b){
-    if(b==0){
-        return a;
+     int calculategcd(int a,int b){
+        if(b==0){
+            return a;
+        }
+        return calculategcd(b,a%b);
     }
-    return calculategcd(b,a%b);
-}
 };
