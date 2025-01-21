ff06bb2400b6bc30471f7d2a8ba71233761f073c
Meghul Parmar <117036095+Meghul@users.noreply.github.com>
2025-01-22 01:37:11 +0530

    Time: 2 ms (32.2%), Space: 60 MB (81.43%) - LeetHub

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int left=0;
+        int right=0;
+        int k=1;
+        int maxlength=0;
+        while(right<nums.size()){
+            if(nums[right]==0){
+                k--;
+            }
+            right++;
+            while(k<0){
+                if(nums[left]==0){
+                    k++;
+                }
+                left++;
+            }
+            maxlength=max(maxlength,right-(left+1));
+        }
+        return maxlength;
+    }
+};
