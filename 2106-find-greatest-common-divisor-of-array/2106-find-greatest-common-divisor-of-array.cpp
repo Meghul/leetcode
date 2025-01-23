177823d9c078a7b0f20e4028f3b316513aca6548
Meghul Parmar <117036095+Meghul@users.noreply.github.com>
2025-01-23 15:26:46 +0530

    Time: 0 ms (100%), Space: 16.3 MB (28.72%) - LeetHub

--- 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.cpp
+++ 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
+    int ans(int a,int b){
+        while(b!=0){
+            int temp=b;
+            b=a%b;
+            a=temp;
+        }
+        return a;
+    }
     int findGCD(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        return gcd(nums[0],nums[nums.size()-1]);
+    
+        return ans(nums[0],nums[nums.size()-1]);
     }
 };
